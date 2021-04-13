e7935529eab1e2be20ca0024560fca4f48a9f1e5
M Afzaal Afzal <afzaal1721@gmail.com>
2021-04-13 08:14:37 +0500

    Footer logo done

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import {Box, Container, Stack} from "@chakra-ui/react";
 import BodyText from "../Typography/BodyText";
 import FooterButton from "../Buttons/FooterButton";
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
     return (
-        <Box p={['8', '12', '16', '24']} px={['2', '4']} bg={'brand.secondary'}>
+        <Box p={['8', '12', '16']} px={['2', '4']} bg={'brand.secondary'}>
             <Container maxW={'container.xl'}>
                 <Box align={'center'}>
                     <BodyText>
@@ -21,6 +22,20 @@ const Footer: React.FC = () => {
                         <FooterButton>About Us</FooterButton>
                         <FooterButton>FAQ</FooterButton>
                     </Stack>
+                </Box>
+                <Box align={'center'} mt={'2rem'}>
+                    <Box borderWidth={'5px'}
+                         bg={'white'}
+                         borderColor={'brand.primary'}
+                         rounded={'xl'} boxShadow={'lg'}
+                         w={['16.5rem','23rem']} h={['6.5rem','8rem']}
+                         display={'flex'} alignItems={'center'}
+                         justifyContent={'center'}
+                    >
+                        <Box width={['14rem','20rem']} h={['3.5rem','5rem']} position={'relative'}>
+                            <Image src={'/home/footer-logo.png'} objectFit={'cover'} layout={'fill'}/>
+                        </Box>
+                    </Box>
                 </Box>
             </Container>
         </Box>
